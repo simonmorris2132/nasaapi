fa4259919da8714764a72ff28a550c5a88216513
Simon Morris <simonjmorris213@gmail.com>
2022-08-08 00:12:42 -0500

    commit

--- src/main/java/com/careerdevs/nasaapi/security/jwt/JwtUtils.java
+++ src/main/java/com/careerdevs/nasaapi/security/jwt/JwtUtils.java
@@ -1,16 +1,12 @@
 package com.careerdevs.nasaapi.security.jwt;
 
 import java.util.Date;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
-
 import com.careerdevs.nasaapi.security.services.UserDetailsImpl;
-
-import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
